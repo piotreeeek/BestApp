a5520f439f6d6447b6210f4e5fa1aae20033c617
Piotrek <piotrek120017@gmail.com>
2017-12-09 15:50:56 +0100

    fsdgfsdg

--- app/src/main/java/com/example/piotrek/bestapp/FormActivity.java
+++ app/src/main/java/com/example/piotrek/bestapp/FormActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -17,9 +18,13 @@ public class FormActivity extends AppCompatActivity {
     public void onButtonSendClick(View view) {
         EditText editText = findViewById(R.id.editText);
         String input = editText.getText().toString();
-        Intent intent = new Intent();
-        intent.putExtra("result", input);
-        setResult(RESULT_OK, intent);
-        finish();
+        if(input.length() > 0) {
+            Intent intent = new Intent();
+            intent.putExtra("result", input);
+            setResult(RESULT_OK, intent);
+            finish();
+        }else{
+            Toast.makeText(this, R.string.input_error, Toast.LENGTH_SHORT).show();
+        }
     }
 }
