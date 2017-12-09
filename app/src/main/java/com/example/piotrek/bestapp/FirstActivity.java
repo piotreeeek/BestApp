cb0955e8125afe85512282fcebfb0909f70c4c5d
Piotrek <piotrek120017@gmail.com>
2017-12-09 15:34:49 +0100

    image add to activity

--- app/src/main/java/com/example/piotrek/bestapp/FirstActivity.java
+++ app/src/main/java/com/example/piotrek/bestapp/FirstActivity.java
@@ -2,14 +2,18 @@ package com.example.piotrek.bestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
 
+    GridLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        layout = findViewById(R.id.first_layout);
         Bundle bundle = getIntent().getExtras();
 
         TextView textView = findViewById(R.id.fa_text_v);
@@ -19,5 +23,9 @@ public class FirstActivity extends AppCompatActivity {
         catch (NullPointerException e){
             textView.setText(R.string.error);
         }
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.kot);
+        layout.addView(imageView);
     }
 }
